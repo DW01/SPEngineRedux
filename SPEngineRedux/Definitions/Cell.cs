5796e55981a4732d7d86714b607b3284b3f83249
DW01 <dark@zejroleplaying.org>
2017-04-24 03:46:41 -0400

    Initial commit.

---
+++ SPEngineRedux/Definitions/Cell.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+using SPEngineRedux.Utilities;
+
+namespace SPEngineRedux.Definitions
+{
+    class Cell
+    {
+        // Cell attributes.
+        // We have to use properties here because hyphens are illegal in C#.
+        [JsonProperty(PropertyName = "cell-name")]
+        public string cellname; // Cell name.
+
+        [JsonProperty(PropertyName = "cell-id")]
+        public char cellid; // Cell Identifier.
+
+        [JsonProperty(PropertyName = "cell-color")]
+        public string cellcolor; // Cell color, in hexadecimal.
+
+
+        // Open the default Cell Bank.
+        public void OpenDefaultCellBank()
+        {
+            JsonUtilities.ReadCellJson("//JsonResources/cell_bank.json");
+        }
+    }
+}
---
+++ SPEngineRedux/Utilities/JsonUtilities.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+using SPEngineRedux.Definitions;
+
+namespace SPEngineRedux.Utilities
+{
+    class JsonUtilities
+    {
+        // JSON reader function.
+        public static void ReadCellJson(string filename)
+        {
+            using (StreamReader data_reader = File.OpenText(filename))
+            {
+                string json = data_reader.ReadToEnd();
+                List<Cell> cells = JsonConvert.DeserializeObject<List<Cell>>(json);
+            }
+        }
+    }
+}
