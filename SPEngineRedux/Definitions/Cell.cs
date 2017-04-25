92d68b54c62a00e4b0a7036020861fffb8f4edd7
DW01 <dark@zejroleplaying.org>
2017-04-24 21:46:49 -0400

    Cell definition, contains all properties for working with the JSON bank through JsonReader and JsonWriter.

--- SPEngineRedux/Definitions/Cell.cs
+++ SPEngineRedux/Definitions/Cell.cs
@@ -13,20 +13,42 @@ namespace SPEngineRedux.Definitions
     {
         // Cell attributes.
         // We have to use properties here because hyphens are illegal in C#.
+
+        // Cell descriptors.
         [JsonProperty(PropertyName = "cell-name")]
-        public string cellname; // Cell name.
+        public string CellName { get; set; } // Cell name.
 
         [JsonProperty(PropertyName = "cell-id")]
-        public char cellid; // Cell Identifier.
+        public string CellID { get; set; } // Cell Identifier.
 
-        [JsonProperty(PropertyName = "cell-color")]
-        public string cellcolor; // Cell color, in hexadecimal.
+        [JsonProperty(PropertyName = "color")]
+        public string Color { get; set; } // Cell color, in hexadecimal.
 
+        [JsonProperty(PropertyName = "classification")]
+        public int Classification { get; set; } // Cell classification. 0 for Empty Cells, 1 for Object Cells, 2 for Units.
 
-        // Open the default Cell Bank.
-        public void OpenDefaultCellBank()
-        {
-            JsonUtilities.ReadCellJson("//JsonResources/cell_bank.json");
-        }
+        [JsonProperty(PropertyName = "alignment")]
+        public int Alignment { get; set; } // Cell alignment. 0 for Envirnment, 1-9 for Team Alignment.
+
+        // Does the Cell exist?
+        [JsonProperty(PropertyName = "exists")]
+        public bool DoesCellExist { get; set; } // Boolean. No need to explain. Largely used for Void or Invisible cells.
+
+        // Cell traversal properties.
+        [JsonProperty(PropertyName = "passable")]
+        public bool IsCellPassable { get; set; } // Boolean. 0 for no, 1 for yes.
+
+        [JsonProperty(PropertyName = "occupiable")]
+        public bool CanOccupyCell { get; set; } // Can we occupy the Cell? 1 for yes, 0 for no.
+
+        [JsonProperty(PropertyName = "destructable")]
+        public bool IsCellDestructible { get; set; } // If the Cell is of type Object, can we destroy it? 1 for YES, 0 for NO.
+
+        [JsonProperty(PropertyName = "block-ranged")]
+        public bool BlocksRangedAttacks { get; set; } // Does the Cell block ranged attacks (Block Property)? 1 for YES, 0 for NO.
+
+        // Cell statistics. Only useful if IsCellDestructible is 1 or Cell is of tye Unit (2).
+        [JsonProperty(PropertyName = "has-stats")]
+        public bool CellHasStats { get; set; } // Are we working with Statistics for this Cell? 1 for YES, 2 for NO.
     }
 }
---
+++ SPEngineRedux/Readers/CellReader.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using SPEngineRedux.Definitions;
+using SPEngineRedux.Utilities;
+
+
+namespace SPEngineRedux.Readers
+{
+    class CellReader
+    {
+        // Open the default Cell Data Bank.
+        public void OpenDefaultCellBank()
+        {
+            JsonUtilities.ReadCellJson("//JsonResources/cell_bank.json");
+        }
+
+        // Open a user-defined Cell Data Bank.
+        public void OpenUserCellBank(string filename)
+        {
+            JsonUtilities.ReadCellJson(filename);
+        }
+    }
+}
